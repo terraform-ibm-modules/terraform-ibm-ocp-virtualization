9ab4b32df6fec1d9179342179a4a69ae4209caed
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-06-19 13:24:56 +0530

    revert test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,14 +2,20 @@
 package test
 
 import (
+	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
 
 // Use existing resource group
@@ -24,46 +30,46 @@ var (
 	permanentResources map[string]interface{}
 )
 
-// type tarIncludePatterns struct {
-// 	excludeDirs []string
+type tarIncludePatterns struct {
+	excludeDirs []string
 
-// 	includeFiletypes []string
+	includeFiletypes []string
 
-// 	includeDirs []string
-// }
+	includeDirs []string
+}
 
-// func getTarIncludePatternsRecursively(dir string, dirsToExclude []string, fileTypesToInclude []string) ([]string, error) {
-// 	r := tarIncludePatterns{dirsToExclude, fileTypesToInclude, nil}
-// 	err := filepath.WalkDir(dir, func(path string, entry fs.DirEntry, err error) error {
-// 		return walk(&r, path, entry, err)
-// 	})
-// 	if err != nil {
-// 		fmt.Println("error")
-// 		return r.includeDirs, err
-// 	}
-// 	return r.includeDirs, nil
-// }
+func getTarIncludePatternsRecursively(dir string, dirsToExclude []string, fileTypesToInclude []string) ([]string, error) {
+	r := tarIncludePatterns{dirsToExclude, fileTypesToInclude, nil}
+	err := filepath.WalkDir(dir, func(path string, entry fs.DirEntry, err error) error {
+		return walk(&r, path, entry, err)
+	})
+	if err != nil {
+		fmt.Println("error")
+		return r.includeDirs, err
+	}
+	return r.includeDirs, nil
+}
 
-// func walk(r *tarIncludePatterns, s string, d fs.DirEntry, err error) error {
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if d.IsDir() {
-// 		for _, excludeDir := range r.excludeDirs {
-// 			if strings.Contains(s, excludeDir) {
-// 				return nil
-// 			}
-// 		}
-// 		if s == ".." {
-// 			r.includeDirs = append(r.includeDirs, "*.tf")
-// 			return nil
-// 		}
-// 		for _, includeFiletype := range r.includeFiletypes {
-// 			r.includeDirs = append(r.includeDirs, strings.ReplaceAll(s+"/*"+includeFiletype, "../", ""))
-// 		}
-// 	}
-// 	return nil
-// }
+func walk(r *tarIncludePatterns, s string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if d.IsDir() {
+		for _, excludeDir := range r.excludeDirs {
+			if strings.Contains(s, excludeDir) {
+				return nil
+			}
+		}
+		if s == ".." {
+			r.includeDirs = append(r.includeDirs, "*.tf")
+			return nil
+		}
+		for _, includeFiletype := range r.includeFiletypes {
+			r.includeDirs = append(r.includeDirs, strings.ReplaceAll(s+"/*"+includeFiletype, "../", ""))
+		}
+	}
+	return nil
+}
 
 // TestMain will be run before any parallel tests, used to set up a shared InfoService object to track region usage
 // for multiple tests
@@ -82,58 +88,58 @@ func TestMain(m *testing.M) {
 /*******************************************************************
 * TESTS FOR THE TERRAFORM BASED QUICKSTART DEPLOYABLE ARCHITECTURE *
 ********************************************************************/
-// func TestRunQuickstartDASchematics(t *testing.T) {
-// 	t.Parallel()
+func TestRunQuickstartDASchematics(t *testing.T) {
+	t.Parallel()
 
-// 	excludeDirs := []string{
-// 		".terraform",
-// 		".docs",
-// 		".github",
-// 		".git",
-// 		".idea",
-// 		"common-dev-assets",
-// 		"examples",
-// 		"tests",
-// 		"reference-architectures",
-// 	}
-// 	includeFiletypes := []string{
-// 		".tf",
-// 		".yaml",
-// 		".py",
-// 		".tpl",
-// 		".md",
-// 		".sh",
-// 	}
+	excludeDirs := []string{
+		".terraform",
+		".docs",
+		".github",
+		".git",
+		".idea",
+		"common-dev-assets",
+		"examples",
+		"tests",
+		"reference-architectures",
+	}
+	includeFiletypes := []string{
+		".tf",
+		".yaml",
+		".py",
+		".tpl",
+		".md",
+		".sh",
+	}
 
-// 	tarIncludePatterns, recurseErr := getTarIncludePatternsRecursively("..", excludeDirs, includeFiletypes)
-// 	// if error producing tar patterns (very unexpected) fail test immediately
-// 	require.NoError(t, recurseErr, "Schematic Test had unexpected error traversing directory tree")
+	tarIncludePatterns, recurseErr := getTarIncludePatternsRecursively("..", excludeDirs, includeFiletypes)
+	// if error producing tar patterns (very unexpected) fail test immediately
+	require.NoError(t, recurseErr, "Schematic Test had unexpected error traversing directory tree")
 
-// 	// Set up a schematics test
-// 	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
-// 		Testing:            t,
-// 		TarIncludePatterns: tarIncludePatterns,
-// 		TemplateFolder:     quickStartTerraformDir,
-// 		// This is the resource group that the workspace will be created in
-// 		ResourceGroup:          resourceGroup,
-// 		Prefix:                 "virt-qs",
-// 		Tags:                   []string{"test-schematic"},
-// 		DeleteWorkspaceOnFail:  false,
-// 		WaitJobCompleteMinutes: 180,
-// 	})
+	// Set up a schematics test
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing:            t,
+		TarIncludePatterns: tarIncludePatterns,
+		TemplateFolder:     quickStartTerraformDir,
+		// This is the resource group that the workspace will be created in
+		ResourceGroup:          resourceGroup,
+		Prefix:                 "virt-qs",
+		Tags:                   []string{"test-schematic"},
+		DeleteWorkspaceOnFail:  false,
+		WaitJobCompleteMinutes: 180,
+	})
 
-// 	// Pass required variables
-// 	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
-// 		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
-// 		// use options.Prefix here to generate a unique prefix every time so resource group name is unique for every test
-// 		{Name: "prefix", Value: options.Prefix, DataType: "string"},
-// 		{Name: "existing_resource_group_name", Value: resourceGroup, DataType: "string"},
-// 		{Name: "machine_type", Value: "bx2.4x16", DataType: "string"},
-// 	}
+	// Pass required variables
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		// use options.Prefix here to generate a unique prefix every time so resource group name is unique for every test
+		{Name: "prefix", Value: options.Prefix, DataType: "string"},
+		{Name: "existing_resource_group_name", Value: resourceGroup, DataType: "string"},
+		{Name: "machine_type", Value: "bx2.4x16", DataType: "string"},
+	}
 
-// 	err := options.RunSchematicTest()
-// 	assert.NoError(t, err, "Schematic Test had unexpected error")
-// }
+	err := options.RunSchematicTest()
+	assert.NoError(t, err, "Schematic Test had unexpected error")
+}
 
 func TestRunQuickstartDAUpgrade(t *testing.T) {
 	t.Parallel()
@@ -153,30 +159,6 @@ func TestRunQuickstartDAUpgrade(t *testing.T) {
 	}
 
 	output, err := options.RunTestUpgrade()
-	if !options.UpgradeTestSkipped {
-		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
-	}
-}
-
-func TestRunQuickstartDATest(t *testing.T) {
-	t.Parallel()
-
-	// Set up upgrade test
-	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-		Testing:      t,
-		TerraformDir: quickStartTerraformDir,
-		Prefix:       "virt-qs",
-	})
-
-	// Pass required variables (NOTE: ibmcloud_api_key is passed directly in test as TF_VAR so no need to include here)
-	options.TerraformVars = map[string]interface{}{
-		"prefix":                       options.Prefix,
-		"existing_resource_group_name": resourceGroup,
-		"machine_type":                 "bx2.4x16",
-	}
-
-	output, err := options.RunTestConsistency()
 	if !options.UpgradeTestSkipped {
 		assert.Nil(t, err, "This should not have errored")
 		assert.NotNil(t, output, "Expected some output")
