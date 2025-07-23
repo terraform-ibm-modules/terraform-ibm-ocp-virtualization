3c835b58c1d2ff04fb84f98dff4cc4351420a897
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-07-23 10:36:54 +0530

    update variable

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -70,86 +70,87 @@ variable "workloads_node_selectors" {
     values = ["physical"]
   }]
 }
-# tflint-ignore: all
-variable "addons" {
-  type = object({
-    debug-tool = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    image-key-synchronizer = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    openshift-data-foundation = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    vpc-file-csi-driver = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    static-route = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    cluster-autoscaler = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    vpc-block-csi-driver = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    ibm-storage-operator = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-    openshift-ai = optional(object({
-      version         = optional(string)
-      parameters_json = optional(string)
-    }))
-  })
-  description = "Map of OCP cluster add-on versions to install (NOTE: The 'vpc-block-csi-driver' add-on is installed by default for VPC clusters and 'ibm-storage-operator' is installed by default in OCP 4.15 and later, however you can explicitly specify it here if you wish to choose a later version than the default one). For full list of all supported add-ons and versions, see https://cloud.ibm.com/docs/containers?topic=containers-supported-cluster-addon-versions. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-ocp-virtualization/blob/main/solutions/quickstart/DA_docs.md#options-with-addons)"
-  nullable    = false
-  # See, https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6290
-  default = {
-    openshift-data-foundation = {
-      parameters_json = "{\"osdStorageClassName\":\"localblock\",\"odfDeploy\":\"true\",\"autoDiscoverDevices\":\"true\"}"
-    }
-    vpc-file-csi-driver = {
-      version = "2.0"
-    }
-  }
-}
 
 # # tflint-ignore: all
-variable "prefix" {
-  type        = string
-  nullable    = true
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-openshift. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
+# variable "addons" {
+#   type = object({
+#     debug-tool = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     image-key-synchronizer = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     openshift-data-foundation = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     vpc-file-csi-driver = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     static-route = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     cluster-autoscaler = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     vpc-block-csi-driver = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     ibm-storage-operator = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#     openshift-ai = optional(object({
+#       version         = optional(string)
+#       parameters_json = optional(string)
+#     }))
+#   })
+#   description = "Map of OCP cluster add-on versions to install (NOTE: The 'vpc-block-csi-driver' add-on is installed by default for VPC clusters and 'ibm-storage-operator' is installed by default in OCP 4.15 and later, however you can explicitly specify it here if you wish to choose a later version than the default one). For full list of all supported add-ons and versions, see https://cloud.ibm.com/docs/containers?topic=containers-supported-cluster-addon-versions. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-ocp-virtualization/blob/main/solutions/quickstart/DA_docs.md#options-with-addons)"
+#   nullable    = false
+#   # See, https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6290
+#   default = {
+#     openshift-data-foundation = {
+#       parameters_json = "{\"osdStorageClassName\":\"localblock\",\"odfDeploy\":\"true\",\"autoDiscoverDevices\":\"true\"}"
+#     }
+#     vpc-file-csi-driver = {
+#       version = "2.0"
+#     }
+#   }
+# }
 
-  validation {
-    # - null and empty string is allowed
-    # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
-    # - Starts with a lowercase letter: [a-z]
-    # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-)
-    # - Must not end with a hyphen (-): [a-z0-9]
-    condition = (var.prefix == null || var.prefix == "" ? true :
-      alltrue([
-        can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)),
-        length(regexall("--", var.prefix)) == 0
-      ])
-    )
-    error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It must not end with a hyphen('-'), and cannot contain consecutive hyphens ('--')."
-  }
+# # # tflint-ignore: all
+# variable "prefix" {
+#   type        = string
+#   nullable    = true
+#   description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-openshift. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
 
-  validation {
-    # must not exceed 16 characters in length
-    condition     = length(var.prefix) <= 16
-    error_message = "Prefix must not exceed 16 characters."
-  }
-}
+#   validation {
+#     # - null and empty string is allowed
+#     # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
+#     # - Starts with a lowercase letter: [a-z]
+#     # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-)
+#     # - Must not end with a hyphen (-): [a-z0-9]
+#     condition = (var.prefix == null || var.prefix == "" ? true :
+#       alltrue([
+#         can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)),
+#         length(regexall("--", var.prefix)) == 0
+#       ])
+#     )
+#     error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It must not end with a hyphen('-'), and cannot contain consecutive hyphens ('--')."
+#   }
+
+#   validation {
+#     # must not exceed 16 characters in length
+#     condition     = length(var.prefix) <= 16
+#     error_message = "Prefix must not exceed 16 characters."
+#   }
+# }
 
 # # tflint-ignore: all
 # variable "default_worker_pool_workers_per_zone" {
@@ -170,25 +171,25 @@ variable "prefix" {
 #   }
 # }
 
-# tflint-ignore: all
-variable "additional_worker_pools" {
-  type = list(object({
-    vpc_subnets = optional(list(object({
-      id         = string
-      zone       = string
-      cidr_block = string
-    })), [])
-    pool_name                     = string
-    machine_type                  = string
-    workers_per_zone              = number
-    operating_system              = string
-    labels                        = optional(map(string))
-    minSize                       = optional(number)
-    secondary_storage             = optional(string)
-    maxSize                       = optional(number)
-    enableAutoscaling             = optional(bool)
-    additional_security_group_ids = optional(list(string))
-  }))
-  description = "List of additional worker pools with custom configurations to accommodate diverse VM workload requirements within the cluster. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-ocp-virtualization/blob/main/solutions/fully-configurable/DA_docs.md#options-with-worker-pools)"
-  default     = []
-}
+# # tflint-ignore: all
+# variable "additional_worker_pools" {
+#   type = list(object({
+#     vpc_subnets = optional(list(object({
+#       id         = string
+#       zone       = string
+#       cidr_block = string
+#     })), [])
+#     pool_name                     = string
+#     machine_type                  = string
+#     workers_per_zone              = number
+#     operating_system              = string
+#     labels                        = optional(map(string))
+#     minSize                       = optional(number)
+#     secondary_storage             = optional(string)
+#     maxSize                       = optional(number)
+#     enableAutoscaling             = optional(bool)
+#     additional_security_group_ids = optional(list(string))
+#   }))
+#   description = "List of additional worker pools with custom configurations to accommodate diverse VM workload requirements within the cluster. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-ocp-virtualization/blob/main/solutions/fully-configurable/DA_docs.md#options-with-worker-pools)"
+#   default     = []
+# }
