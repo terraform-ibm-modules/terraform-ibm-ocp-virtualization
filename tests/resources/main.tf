e5284408660f8763cd4c9b819528dbbba71eb510
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-08-19 10:48:43 +0530

    update

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -82,21 +82,21 @@ locals {
 
 module "ocp_base" {
   source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.49.0"
+  version                             = "3.55.2"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.resource_tags
   cluster_name                        = local.cluster_name
   force_delete_storage                = true
   vpc_id                              = ibm_is_vpc.vpc.id
-  ocp_version                         = "4.17"
+  ocp_version                         = "4.18"
   vpc_subnets                         = local.cluster_vpc_subnets
   worker_pools                        = local.worker_pools
   access_tags                         = []
   disable_outbound_traffic_protection = true # set as True to enable outbound traffic; required for accessing Operator Hub in the OpenShift console.
   addons = {
     openshift-data-foundation = {
-      version         = "4.17.0"
+      version         = "4.18.0"
       parameters_json = <<PARAMETERS_JSON
         {
             "osdStorageClassName":"localblock",
