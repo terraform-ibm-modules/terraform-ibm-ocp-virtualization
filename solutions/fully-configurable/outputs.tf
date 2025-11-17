2a407371479b950f5ec2981e63527ea1a43923b2
Harika-Ponna <Harika.Ponna@ibm.com>
2025-11-17 17:45:11 +0530

    feat: added next step urls for the DA

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -16,3 +16,19 @@ output "cluster_crn" {
   description = "CRN of the cluster."
   value       = module.virtualization.cluster_crn
 }
+
+output "next_steps_text" {
+  value       = "Your Red Hat OpenShift environment is ready."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Cluster"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/containers/cluster-management/clusters/${module.virtualization.cluster_id}/overview"
+  description = "Primary URL"
+}
+
