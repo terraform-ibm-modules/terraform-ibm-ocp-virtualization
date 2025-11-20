b87334f613f068d6f2934a4b688b940a80db4f48
Harika-Ponna <Harika.Ponna@ibm.com>
2025-11-20 10:20:06 +0530

    feat: added next step urls for the DA

--- solutions/quickstart/outputs.tf
+++ solutions/quickstart/outputs.tf
@@ -71,3 +71,18 @@ output "master_status" {
   description = "The current status of the Kubernetes master node in the cluster."
   value       = module.ocp_base.master_status
 }
+
+output "next_steps_text" {
+  value       = "Your Red Hat OpenShift environment is ready."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Cluster"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/containers/cluster-management/clusters/${module.ocp_base.cluster_id}/overview"
+  description = "Primary URL"
+}
