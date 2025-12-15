b5eac9aa47fa587a6f18727fb21e81fe1ca90603
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-12-15 22:45:40 +0530

    test

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -81,8 +81,9 @@ locals {
 }
 
 module "ocp_base" {
-  source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.74.0"
+  #checkov:skip=CKV_TF_2: TEst
+  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-base-ocp-vpc?ref=test-bin"
+  # version                             = "3.74.0"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.resource_tags
