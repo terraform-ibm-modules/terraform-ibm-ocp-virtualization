8829a59f1839c1894b126d1429ffb15c3bb66bf5
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-03 22:50:40 +0000

    fix(deps): update terraform terraform-ibm-modules/base-ocp-vpc/ibm to v3.76.3

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -102,7 +102,7 @@ locals {
 
 module "ocp_base" {
   source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.76.2"
+  version                             = "3.76.3"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.cluster_resource_tags
