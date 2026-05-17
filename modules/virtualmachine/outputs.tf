0dc82dc82eb953cf64ff80669d34e30519c40bd1
Vipin Kumar <kumarvipin88073@gmail.com>
2026-05-17 20:09:23 +0530

    fix

--- modules/virtualmachine/outputs.tf
+++ modules/virtualmachine/outputs.tf
@@ -16,19 +16,3 @@ output "vm_status" {
   description = "Status of the Helm release."
   value       = helm_release.vm.status
 }
-
-output "helm_release_version" {
-  description = "Version of the Helm release."
-  value       = helm_release.vm.version
-}
-
-output "vm_access_info" {
-  description = "Information for accessing the VM."
-  value = {
-    vm_name        = var.vm_name
-    namespace      = var.vm_namespace
-    username       = var.vm_username
-    ssh_enabled    = var.expose_ssh_service
-    access_command = var.expose_ssh_service ? "kubectl get svc ${var.vm_name}-ssh -n ${var.vm_namespace}" : "virtctl console ${var.vm_name} -n ${var.vm_namespace}"
-  }
-}
