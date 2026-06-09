2dd5eb5770993909914f86f55c15fac7193c3d05
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-06-09 05:50:41 +0000

    fix(deps): update dependencies

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -86,7 +86,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.87.4"
+  version              = "3.87.5"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
