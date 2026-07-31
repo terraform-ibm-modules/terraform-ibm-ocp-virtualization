50a0dc0fe00b9ff836eaf13d87eecaa078019709
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-31 22:55:18 +0000

    fix(deps): update dependencies

--- scripts/install-binaries.sh
+++ scripts/install-binaries.sh
@@ -7,7 +7,7 @@ set -o pipefail
 DIRECTORY=${1:-"/tmp"}
 export PATH=$PATH:$DIRECTORY
 # renovate: datasource=github-tags depName=terraform-ibm-modules/common-bash-library
-TAG=v0.4.0
+TAG=v0.5.0
 TMP_DIR=$(mktemp -d "${DIRECTORY}/common-bash-XXXXX")
 
 echo "Downloading common-bash-library version ${TAG}."
